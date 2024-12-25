918d069889693e85f6c98b2774edf2c4c308b2ac
jinjae <leejinjae7@gmail.com>
2024-12-25 22:43:46 +0900

    section 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import BottomNavitaion from "@/components/navigation/BottomNavigation";
+import TopNavigation from "@/components/navigation/TopNavigation";
+import MainSection from "@/components/section/MainSection";
+
 export default function Home() {
-  return <div>hello world</div>;
+  return (
+    <main className="h-screen">
+      <TopNavigation />
+      <MainSection />
+      <BottomNavitaion />
+    </main>
+  );
 }
---
+++ components/navigation/TopNavigation.tsx
@@ -0,0 +1,11 @@
+"use client";
+export default function TopNavigation() {
+  return (
+    <nav className="h-[10%]">
+      <div className="flex flex-col w-full justify-center items-center border rounded-b-xl bg-teal-300/80">
+        <div className="mt-4">Address</div>
+        <div>Search bar</div>
+      </div>
+    </nav>
+  );
+}
